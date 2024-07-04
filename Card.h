71573327ed95523b82040fa089dcafed2a21a9c1
Mihael Krpes <mikrpes@vsite.hr>
2024-07-04 10:27:53 +0200

    Add project files.

---
+++ Card.h
@@ -0,0 +1,16 @@
+#pragma once
+
+enum class CardColors
+{
+	Red, Yellow,Blue,Green,Black
+};
+enum class CardTypes
+{
+	N0,N1,N2,N3,N4,N5,N6,N7,N8,N9,Skip,Reverse,DrawTwo,ColorChange,DrawFour
+};
+class Card
+{
+	Card() {
+	}
+};
+
